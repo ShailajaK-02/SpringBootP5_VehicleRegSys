63619dfe9e626f18a4a0085cb005bedab0ba39c9
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-10 14:45:32 +0530

    main class implemented

--- src/main/java/com/registration/VehicleRegistrationSystemApplication.java
+++ src/main/java/com/registration/VehicleRegistrationSystemApplication.java
@@ -2,7 +2,7 @@ package com.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ //main class
 @SpringBootApplication
 public class VehicleRegistrationSystemApplication {
 
